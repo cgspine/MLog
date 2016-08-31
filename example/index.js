16403891bf4e6cb44483c19917f84e79da2d5283
cgspine <cgspine@gmail.com>
2016-08-31 14:24:59 +0800

    format

--- example/index.js
+++ example/index.js
@@ -7,8 +7,11 @@ var MLog = require('../lib')
 var logger = MLog.get()
 var fileTarget = new MLog.File({
     logDir: __dirname,
-    filename: 'log'
+    filename: 'log',
+    rollIntervals: 24*60*60,
+    rollMiniSize: 1024
 })
+logger.removeTarget('console')
 logger.addTarget(fileTarget)
 
 for(var i=0;i<100;i++) {
