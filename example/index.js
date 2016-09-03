7cc18dc5f7f5e119f0ab3c4be3ef218b399ce0fe
cgspine <cgspine@gmail.com>
2016-09-03 17:06:03 +0800

    optimization

--- example/index.js
+++ example/index.js
@@ -5,15 +5,26 @@
 var MLog = require('../lib')
 
 var logger = MLog.get()
-var fileTarget = new MLog.File({
+var fileTarget = new MLog.targets.File({
     logDir: __dirname,
     filename: 'log',
-    rollIntervals: 24*60*60,
-    rollMiniSize: 1024
+    rollIntervals: 24*60*60 * 1000,
+    rollMiniSize: 1024 * 50
 })
-logger.removeTarget('console')
+// logger.removeTarget('console')
 logger.addTarget(fileTarget)
 
-for(var i=0;i<100;i++) {
-    logger.i('demo','haha')
+for(var i=0;i<10;i++) {
+    logger.d('demo','haha')
 }
+
+logger.d('demo',{
+    a:123,
+    b:234,
+    c:145
+},{
+    a:123,
+    b:234,
+    c:145
+})
+logger.d('haha', new Error("error"))
